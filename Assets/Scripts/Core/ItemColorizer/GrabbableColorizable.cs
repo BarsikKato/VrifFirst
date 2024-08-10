bdda3136dae3aff10052c4350c4de0049e3b1598
Barsik_Kato <noran-kotov@yandex.ru>
2024-08-10 13:01:16 +0300

    Grabbable colorizing

---
+++ Assets/Scripts/Core/ItemColorizer/GrabbableColorizable.cs
@@ -0,0 +1,69 @@
+using BNG;
+using System.Collections;
+using UnityEngine;
+
+namespace Core.ItemColorization
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public sealed class GrabbableColorizable : GrabbableEvents
+    {
+        [SerializeField] private Color grabColor;
+        [SerializeField] private float colorizationSpeed = 2f;
+
+        private Coroutine _colorizeCoroutine;
+        private MeshRenderer _renderer;
+
+        private float _animationTime = 1f;
+        private Color _normalColor;
+
+        private void Start()
+        {
+            _renderer = GetComponent<MeshRenderer>();
+            _normalColor = _renderer.material.color;
+        }
+
+        public override void OnGrab(Grabber grabber)
+        {
+            ColorizeTo(grabColor);
+        }
+
+        public override void OnRelease()
+        {
+            ColorizeTo(_normalColor);
+        }
+
+        /// <summary>
+        /// Покрасить предмет в указанный цвет.
+        /// </summary>
+        /// <param name="color">Цвет для покраски.</param>
+        private void ColorizeTo(Color color)
+        {
+            if (_colorizeCoroutine != null)
+            {
+                StopCoroutine(_colorizeCoroutine);
+            }
+
+            _colorizeCoroutine = StartCoroutine(ColorizeToRoutine(color));
+        }
+
+        private IEnumerator ColorizeToRoutine(Color color)
+        {
+            Material rendererMaterial = _renderer.material;
+            Color startingColor = color == _normalColor ? grabColor : _normalColor;
+            
+            // Вычисление нужно в случае, если предыдущая анимация была прервана
+            _animationTime = 1f - _animationTime;
+            while (_animationTime <= 1f)
+            {
+                rendererMaterial.color = Color.Lerp(
+                    startingColor, color, _animationTime);
+
+                _animationTime += Time.deltaTime * colorizationSpeed;
+                yield return null;
+            }
+
+            _animationTime = 1f;
+            _colorizeCoroutine = null;
+        }
+    }
+}
